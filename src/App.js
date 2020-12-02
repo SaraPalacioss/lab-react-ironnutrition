a95771c03e1023683e7feec8abe2a8aed732d8b0
sarastackhouse <sarapalacios7@gmail.com>
2020-12-02 19:02:13 +0100

    Iteration 1-4 done

--- src/App.js
+++ src/App.js
@@ -2,29 +2,110 @@ import React from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
-import Foodbox from './components/Foodbox';
-
-
-
+import FoodBox from './components/FoodBox';
+import Search from './components/Search';
 
 class App extends React.Component {
-  state={
-    foodsList: foods
+	state = {
+		food: [ ...foods ],
+		searchedWord: '',
+		temporalNewFood: { name: '', calories: '', image: '', quantity: 0 },
+		showForm: false
+	};
 
-  };
+	changeSearchedWord = (_value) => {
+		this.setState({ searchedWord: _value });
+	};
 
-render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Foodbox 
-        foodsList={this.state.foodsList}
-        />
-      </header>
-       
-    </div>
-  );
-}
+	checkForIncludedFood = () => {
+		const filteredFoods = this.state.food.filter((food) => {
+			return food.name.toLowerCase().includes(this.state.searchedWord.toLowerCase());
+		});
+		return filteredFoods;
+	};
+
+	renderFood = () => {
+		const finalArrayOfFoods = this.checkForIncludedFood();
+		return finalArrayOfFoods.map((food, index) => {
+			return <FoodBox key={index} name={food.name} calories={food.calories} image={food.image} />;
+		});
+	};
+
+	submitForm = (event) => {
+		event.preventDefault();
+		const copyOfFoods = [ ...this.state.food ];
+		copyOfFoods.unshift(this.state.temporalNewFood);
+
+    this.setState({ food: copyOfFoods,  showForm: false});
+    
+    // axios.post('/create-new-food', this.state.temporalNewFood)
+	};
+
+	renderForm = () => {
+		return (
+			<form onSubmit={this.submitForm}>
+				<label htmlFor="name">Name: </label>
+				<input
+					type="text"
+					name="name"
+					onChange={(event) =>
+						this.setState({ temporalNewFood: { ...this.state.temporalNewFood, name: event.target.value } })}
+				/>
+
+				<label htmlFor="calories">Calories: </label>
+				<input
+					type="text"
+					name="calories"
+					onChange={(event) =>
+						this.setState({
+							temporalNewFood: { ...this.state.temporalNewFood, calories: event.target.value }
+						})}
+				/>
+
+				<label htmlFor="image">Image: </label>
+				<input
+					type="text"
+					name="image"
+					onChange={(event) =>
+						this.setState({
+							temporalNewFood: { ...this.state.temporalNewFood, image: event.target.value }
+						})}
+				/>
+
+				<button type="submit">Create</button>
+			</form>
+		);
+	};
+
+	render() {
+		return (
+			<div className="App">
+    
+     
+      <button onClick={() => this.setState({showForm: true})}>Add new food</button>
+
+{this.state.showForm && this.renderForm()}
+
+<Search changeSearchedWord={this.changeSearchedWord} />
+<div className="columns">
+<div className="column">
+{this.renderFood()}
+
+      </div>
+      <div className="column">
+<h2>Today's foods</h2>
+<ul>
+  <li></li>
+</ul>
+<p>Total: ___ cal</p>
+
+      </div>
+      
+      </div>
+
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Foodbox.js
+++ src/components/Foodbox.js
@@ -1,44 +1,37 @@
-import React from "react";
+import React from 'react'
 
-const Foodbox = (props) => {
-    return props.foodsList.map((foods) => {
-  
-
-    return (
-        <div className="box">
-        <article className="media">
-          <div className="media-left">
-            <figure className="image is-64x64">
-              <img src={foods.image} alt={foods.image}/>
-            </figure>
+const FoodBox = (props) => {
+  return(
+    <div className="box">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src={props.image} alt={props.name}/>
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{props.name}</strong> <br />
+              <small>{props.calories} cal</small>
+            </p>
           </div>
-          <div className="media-content">
-            <div className="content">
-            <div></div>
-              <p>
-                <strong>{foods.name}</strong> <br />
-                <small>{foods.calories}</small>
-              </p>
+        </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input className="input" type="number" value="1" />
+            </div>
+            <div className="control">
+              <button className="button is-info">
+                +
+              </button>
             </div>
           </div>
-          <div className="media-right">
-            <div className="field has-addons">
-              <div className="control">
-                <input className="input" type="number" value="1" />
-              </div>
-              <div className="control">
-                <button className="button is-info">
-                  +
-                </button>
-              </div>
-            </div>
-          </div>
-        </article>
-      </div>
-    )
-})
+        </div>
+      </article>
+    </div>
+  )
 }
-  
-// };
 
-export default Foodbox;
+export default FoodBox
