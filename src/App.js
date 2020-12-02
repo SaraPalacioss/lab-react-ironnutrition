dc38ea9488cbc85a55e1cb00edebb81bd8dffed6
sarastackhouse <sarapalacios7@gmail.com>
2020-12-02 11:23:49 +0100

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
+import Foodbox from './components/Foodbox';
 
 
 
@@ -11,19 +11,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Foodbox />
       </header>
+       
+       
+   
     </div>
   );
 }
